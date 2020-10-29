81aba1d0644ac5a0e2636e69d318a19922e8372a
Lasang Jimba Tamang <ljtamang@memphis.edu>
2020-10-29 13:30:48 -0500

    Create Task1.java

---
+++ MainTask/Task1.java
@@ -0,0 +1,11 @@
+public class MyTester {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 8;
+        int result;
+
+        int k = j * i;
+        result = k / 7 % 2;
+        System.out.println(result);
+    }
+}
