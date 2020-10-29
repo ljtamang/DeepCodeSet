fa9ed8cc9492c4107abeed4ae2c0ce8f2ac0582b
Lasang Jimba Tamang <ljtamang@memphis.edu>
2020-10-29 13:33:37 -0500

    Create Task4.java

---
+++ MainTask/Task4.java
@@ -0,0 +1,9 @@
+public class Tester {
+    public static void main(String[] args) {
+        int result = 2;
+        for (int i = 0; i < 5; i++) {
+            result = result + i;
+        }
+        System.out.println(result);
+    }
+}
