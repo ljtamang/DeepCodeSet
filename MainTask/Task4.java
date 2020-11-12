44133c21c7cfec1645c21fce8067e7f0de8551fe
pritiOli <ollipriti@gmail.com>
2020-11-12 14:32:58 -0600

    fixed some typos

--- MainTask/Task4.java
+++ MainTask/Task4.java
@@ -24,26 +24,26 @@ public class Tester {
         int result = 2;
 
         /**
-         * logical_step_2: Add the value of natural numbers less than 5 to the previous value of result
+         * logical_step_2: Add the value of natural numbers less than 5 to the previous value of result.
          * logical_step_detail: Add the value of each natural number which is less than 5 to the previous value of the result
-         * and update the value of the variable result
+         * and update the value of the variable result.
          * question_1: What does the following block of code do?
-         * answer_1: Add the value of natural numbers less than 5 to the previous value of result
+         * answer_1: Add the value of natural numbers less than 5 to the previous value of result.
          * question_2: How many times is the body of for-loop executed?
          * answer_2: The body of the for-loop is executed 5 times.
          * question_3: What is the value of  result when i=1?
          * answer_3:  the value of result is 3.
-         * question_4: what is initial value of the variable i?
+         * question_4: What is initial value of the variable i?
          * answer_4:  the initial value of i is 0.
-         * question_5: what is the loop termination criteria for the for-loop given below
+         * question_5: What is the loop termination criteria for the for-loop given below?
          * answer_5:  i >= 5
          */
 
         /**
-         *  stm_comment: The for loop has initial value i=10, it checks on whether the value of i is less than 5. If true
+         *  stm_comment: The for loop has initial value i=10, it checks on whether the value of i is less than 5. If true,
          *  the body of the for loop is executed and the value of the variable result is updated by adding the value of i to it.
-         *  question_1:Please explain what the following for loop does.
-         *  answer_1: The for loop has initial value i=10, it checks on whether the value of i is less than 5. If true
+         *  question_1: Please explain what the following for loop does.
+         *  answer_1: The for loop has initial value i=10, it checks on whether the value of i is less than 5. If true,
          *  the body of the for loop is executed and the value of the variable result is updated by adding the value i to it.
          * */
 
