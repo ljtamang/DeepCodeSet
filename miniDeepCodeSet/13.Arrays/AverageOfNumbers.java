07c64f900b6ec8bd665c6f47c409ddec801c058a
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-06 19:16:19 -0500

    Update AverageOfNumbers.java

--- miniDeepCodeSet/13.Arrays/AverageOfNumbers.java
+++ miniDeepCodeSet/13.Arrays/AverageOfNumbers.java
@@ -1,7 +1,7 @@
 /**
  * author: Lasang Tamang
  * topic: Arrays
- * subTopic: declaring array variables, processing array
+ * subTopic: array declaration, array initalization, accessing value of an array
  * goalDescription: Calculate average of numbers.
  * source: https://www.programiz.com/java-programming/examples/average-arrays
  * input: N/A
@@ -11,55 +11,64 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
 
         /**
-         * logical_step_1: Declares variables to store numbers and their sum.
-         * logical_step_details: An array of double types called numArray is declared to store the numbers of which we want to find the average. Also, a variable called the sum of double type is declared to represent the sum of the numbers. The value of the sum is initialized to 0.
-         * question_1: What does the following block of code do?
-         * answer_1: Declares variables to store numbers and their sum.
-         * question_2: What variables are declared?
-         * answer_2: An array of double types called numArray and a variable called the sum of double type is declared.
-         * question_3: What is the value of the sum initialized to?
-         * answer_3: The value of the sum is initialized to 0.
+         * domain_model_question: What is the goal of the following block of code?
+         * logical_step_1: Declares variables to store numbers, sum and the average.
+         * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
+         * logical_step_details: An array numArray of double type is declared to store numbers. The array numArray is initialized to the value of 8, 6, 11, and 7. The variable sum of double type is declared to hold the sum of the numbers. The value of the sum is initialized to 0. The variable average of double type is declared to hold the value of the average.
+         * question_1: What is done to store numbers?
+         * answer_1: An array numArray of double types is declared to store numbers.
+         * question_2: What value is the array numArray initialized to?
+         * answer_2: The array numArray is initialized to value of 8, 6, 11, and 7
+         * question_3: What is the variable sum of a double type declared to do?
+         * answer_3: The variable sum of double type is declared to hold the sum of the numbers.
+         * question_4: What is the value of the sum initialized to?
+         * answer_4: The value of the sum is initialized to 0.
+         * question_5: What is the variable average of a double type declared to do?
+         * answer_5: The variable average of double type is declared to hold the value of the average.
          */
         double[] numArray = {8, 6, 11, 7};
         double sum = 0.0;
+        double average;
 
         /**
-         * logical_step_2: Calculate the sum of all elements in the array.
-         * logical_step_details: The sum of all elements in the array is calculated using a for-each loop. The for-each loop iterates over each value of the array and adds that value to the sum. At the end of the for-each loop execution, the value of the sum is 32.
-         * question_1: What does the following block of code do?
-         * answer_1: Calculate the sum of all elements in the array.
-         * question_2: What does for-each loop do?
-         * answer_2: Calculates the sum of all elements in the array.
-         * question_3: What is the value of sum when the for-each loop completes execution?
-         * answer_3: When the for-each loop completes execution, the value of the sum 32.
+         * domain_model_question: What is the goal of the following block of code?
+         * logical_step_2: Calculate the sum of numbers.
+         *  program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
+         * logical_step_details: The for loop is used to iterate over each value of the array. During each iteration, the value of the array at index i is added to the value of the sum. When the for loop completes, the value of the sum is 32.
+         * question_1: What does the for loop do?
+         * answer_2: The for loop is used to iterate over each value of the array.
+         * question_2: What happens during each iteration?
+         * answer_2: During each iteration, the value of the array at index i is added to the value of the sum.
+         * question_3: What is the value of the sum when the for loop completes?
+         * answer_3: When the for loop completes, the value of the sum is 32.
          */
         /**
-         * stm_comment: The for loop is used to iterate over each element of the array.
-         * question_1: what is the for loop used to do?
-         * answer_1:The for loop is used to iterate over each element of the array.
+         * stm_comment: numArray.length is used to get the length of the array.
+         * question_1: What is numArray.length used for?
+         * answer_1: numArray.length is used to get the length of the array.
          */
-        for (double num: numArray) {
-            sum += num;
+        for (int i = 0; i < numArray.length; i++) {
+            /**
+             * stm_comment: numArray[i] is used to acces array element at index i.
+             * question_1:  What is numArray[i] used for?
+             * answer_1: numArray[i] is used to acces array element at index i.
+             */
+            sum += numArray[i];
         }
 
         /**
+         * domain_model_question: What is the goal of the following block of code?
          * logical_step_3: Calculate the average and print it.
-         * logical_step_details: The average of numbers is calculated by dividing the sum of elements of the array by the length of the array. The value of the sum is 32 and the length of the array is 4. Therefore, the calculated value of the average is 8.00.
-         * question_1: What does the following block of code do?
-         * answer_1: Calculate the average and print it.
-         * question_2: How is the average calculated?
-         * answer_2: The average is calculated by dividing the sum of elements of the array by the length of the array.
-         * question_3: What is the length of the array?
-         * answer_3: Length of the array is 5.
-         * question_4: What is the calculated value of the average?
-         * answer_4: The calculated value of the average is 8.00.
+         *  program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
+         * logical_step_details: The average of numbers is calculated by dividing the sum of elements of the array by the length of the array. The length of the array numArray is 5. Therefore, the calculated value of the average is 8.00.
+         * question_1: How is the average calculated?
+         * answer_1: The average is calculated by dividing the sum of elements of the array by the length of the array.
+         * question_2: What is the length of the array?
+         * answer_2: The length of the array numArray is 5.
+         * question_3: What is the calculated value of the average?
+         * answer_3: The calculated value of the average is 8.00.
          */
-        /**
-         * stm_comment: numArray.length gives the number of elements of the array.
-         * question_1: What gives the number of elements of an array?
-         * answer_1: numArray.length gives the number of elements of the array.
-         */
-        double average = sum / numArray.length;
+        average = sum / numArray.length;
         System.out.format("The average is: %.2f", average);
 
     }
