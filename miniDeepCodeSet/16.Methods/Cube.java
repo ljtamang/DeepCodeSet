eddd33221299ecfecb21547aed2e0457ac8ecd63
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-07 20:51:14 -0500

    Update Cube.java

--- miniDeepCodeSet/16.Methods/Cube.java
+++ miniDeepCodeSet/16.Methods/Cube.java
@@ -1,23 +1,61 @@
 /**
- * author: Hasan Mashrique
+ * author: Lasang Tamang
  * topic: Methods
- * subtopic: creating class objects, storing results in a variable, printing variable
+ * subtopic: Define method, calling method
  * goalDescription: Calculate the volume of the cube.
  * source: N/A
  * *input : N/A
  * output: 27
  */
 public class Cube {
+    public static void main(String args[]) {
+
+        /**
+         * domain_model_question: What is the goal of the following block of code?
+         * logical_step_1: Declare variables to store the length and volume of the cube. 
+         *  program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
+         * logical_step_details: A variable sideLength of integer type is declared to represent the length of a side of the cube. The value of the variable sideLength is initialized to 3. The variable vol is declared to store the volume of the cube.
+         * question_1: What is the type of variable sideLength?
+         * answer_1: The variable sideLength is of integer type. 
+         * question_2: What value is the variable sideLength initialized to?
+         * answer_2: The value of the variable sideLength is initialized to 3.
+         * question_3: Why is variable vol declared?
+         * answer_3: The variable vol is declared to store the volume of the cube.
+         */
+        int sideLength = 3;
+        int vol;
+
+        /**
+         * domain_model_question: What is the goal of the following block of code?
+         * logical_step_1: Computes the volume of cube and prints the value.
+         *  program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
+         * logical_step_details: To calculate the volume of the cube, the program calls a method vol. The method vol returns the value 27. Therefore, the program prints 27.
+         * question_1: What does the program do to calculate the volume of the cube?
+         * answer_1: To calculate the volume of the cube, the program calls a method vol.
+         * question_2: What value does method vol return?
+         * answer_2: The method vol returns the value 27.
+         * question_3: What does the program print?
+         * answer_3: The program prints 27.
+         */
+        /**
+         stm_comment: The value 3 is passed as a parameter value to the method cube. 
+         * question_1: What value is passed as a parameter value to method cube?
+         * answer_1: The value 3 is passed as a parameter value to the method cube. 
+         */
+        vol = Cube.vol(sideLength);
+        System.out.println(vol);
+
+    }
 
     /**
-     * logical_step_1:  Declares a method vol to calculate the volume of a cube.
+     * domain_model_question: What is the goal of the following block of code?
+     * logical_step_3: Defines a method vol to calculate the volume of a cube.
+     * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
      * logical_step_details: A static method vol with int x as parameter is defined. The method vol computes and returns the volume of a cube.
-     * question_1: What does the following code block do?
-     * answer_1: Declares a method cube to calculate the volume of a cube.
-     * question_2: What is defined?
-     * answer_2: A static method vol with int x as parameter is defined.
-     * question_3: What does the method vol do?
-     * answer_3: The method vol computes and returns the volume of a cube.
+     * question_1: What is defined?
+     * answer_1: A static method vol with int x as parameter is defined.
+     * question_2: What does the method vol do?
+     * answer_2: The method vol computes and returns the volume of a cube.
      */
     /**
      * stm_comment: The keyword static means it doesn't require a class object to call the method.	
@@ -28,27 +66,4 @@ public class Cube {
         return x * x * x;
     }
 
-    public static void main(String args[]) {
-
-        /**
-         * logical_step_2: Call a method vol and print the returned value. 
-         * logical_step_details: Call the method vol of class Cube. The returned value of the method vol is assigned to a variable result. Finally, the value of the result is printed which is 27.
-         * question_1: What does the following code block do?
-         * answer_1: Call a method vol and print the returned value. 
-         * question_2: What is called?
-         * answer_2: Method vol of class Cube is called.
-         * question_3: What is assigned to a variable result?
-         * answer_3: The returned value of the method vol is assigned to a variable result.
-         * question_4: what value is printed?
-         * answer_4: The value 27 is printed.
-         */
-        /**
-         stm_comment: The value 3 is passed as a parameter value to the method cube. 
-         * question_1: What value is passed as a parameter value to method cube?
-         * answer_1: The value 3 is passed as a parameter value to method cube. 
-         */
-        int result = Cube.vol(3);
-        System.out.println(result);
-
-    }
 }
