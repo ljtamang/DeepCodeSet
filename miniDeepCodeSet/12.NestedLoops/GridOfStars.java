fafd340055318809d30b129f9d09a69f9748bdff
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-18 08:51:27 -0500

    Update GridOfStars.java
    Merged Dr Rus first Feedback

--- miniDeepCodeSet/12.NestedLoops/GridOfStars.java
+++ miniDeepCodeSet/12.NestedLoops/GridOfStars.java
@@ -15,30 +15,30 @@ public class GridOfStars {
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_1: Specify the size of the grid in terms of height and width measured in the number of stars.
+         * logical_step_1: It specifies the size of the grid in terms of height and width measured in the number of stars.
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: Declare two integer variables height and width to indicates the size of the grid. The value of the variable height is initialized to 3. The value of the variable width is initialized to 5.
-         * question_1: What is done to indicate the size of the grid?
-         * answer_1: Declare two integer variables height and width that indicate the size of the grid.
+         * logical_step_details: It declares two integer variables, height, and width, that indicates the size of the grid. The value of the variable height is initialized to 3. The value of the variable width is initialized to 5.
+         * question_1: How exactly is the size of the grid represented computationally?
+         * answer_1: It declares two integer variables, height, and width, that indicates the size of the grid.
          * question_2: What value is the variable height initialized to?
          * answer_2: The value of the variable height is initialized to 3.
          * question_3: What value is the variable width initialized to?
          * answer_3: The value of the variable height is initialized to 5.
-         */
         int height = 3, width = 5;
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_2: Use nested loops to scan the grid and print a star in each cell of the grid.
+         * logical_step_2: It scans the grid and prints a star for each cell of the grid.
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: Uses two for loops to scan the grid one row at a time from left to right and print a star in each cell of the grid. The first for loop is used to scan all rows of the grid using the loop variable row. The second for loop scans all the columns given the current row indicated by the outer loop.
-         * question_1: How many loops do we use?
-         * answer_1: We use two for loops.
-         * question_2: What is the first for loop used for?
-         * answer_2: The first for loop is used to scan all rows of the grid.
-         * question_3: What does the second for loop do?
-         * answer_3: The second for loop scans all the columns given the current row indicated by the outer loop.
+         * logical_step_details: Two for loops are used to scan the grid because the grid is two-dimensional. The first or outer for loop is used to scan all rows of the grid using the loop variable row. The second or inner for loop scans all the columns in the current row using the loop variable col.
+         * question_1: How many loops are used to scan the grid?
+         * answer_1: Two for loops are used to scan the grid because the grid is two-dimensional.
+         * question_2: What is the first or outer for loop for?
+         * answer_2: The first or outer for loop scans all rows of the grid.
+         * question_3: What does the second or inner for loop do?
+         * answer_3: The second or inner for loop scans all the columns in the current row.
          */
+        */
         /** 
          * stm_comment: This first for loop is called the outer loop. The outer loop runs for height iterations.
          * question_1: What is the first for loop called?
