7bfdea60350123fc6da232e9116413db3bdb23e6
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-18 07:44:02 -0500

    Update LeapYear.java
    Updated with Dr Rus second Feedback

--- miniDeepCodeSet/05.LogicalOperators/LeapYear.java
+++ miniDeepCodeSet/05.LogicalOperators/LeapYear.java
@@ -12,15 +12,15 @@ public class LeapYear {
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_1: It declares variables to store and check whether the year is a leap year or not.
+         * logical_step_1: It declares variables to store and check whether the year is a leap year.
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: A variable year of integer type is declared to store a year. The value of the variable year is initialized to 2020. Also, the variable leapYear of string type is defined.
-         * question_1: What is the type of variable year?
-         * answer_1: The variable year is of type integer. 
+         * logical_step_details: A variable year of integer type is declared to store a year. The value of the variable year is initialized to 2020. Also, the variable leapYear of string type is defined to store a message to be displayed indicating whether the year specified is a leap year.
+         * question_1: Why is the variable year declared and what is its type?
+         * answer_1: A variable year of integer type is declared to store a year.
          * question_2: What value is the variable year initialized to?
          * answer_2: The value of the variable year is initialized to 2020.
-         * question_3: What is the type of variable leapYear?
-         * answer_3: The variable leapYear is of type string. 
+         * question_3: Why is variable leapYear declared and what is its type?
+         * answer_3: The variable leapYear of string type is defined to store a message to be displayed indicating whether the year specified is a leap year. 
          */
         int year = 2020;
         String leapYear;
@@ -29,20 +29,22 @@ public class LeapYear {
          * domain_model_question: What is the goal of the following block of code?
          * logical_step_2: It checks and prints whether the year is a leap year or not.
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: The expression is defined using a logical operator to check whether the year is a leap year or not. Since the expression evaluates to true, the value "Leap Year" is assigned to the string variable leapYear. Therefore, the program prints "Leap Year".
-         * question_1: What is done to check whether a year is a leap year or not?
-         * answer_1: The expression is defined using a logical operator to check whether the year is a leap year or not.
-         * question_2: Does the expression evaluate to true?
+         * logical_step_details: An expression is defined using a ternary logical operator to check whether the year is a leap year. Because the expression evaluates to true for the year 2020, the string value "Leap Year" is assigned to the string variable leapYear. Therefore, the program prints to the standard output device the message "Leap Year".
+         * question_1: How is the specified year checked whether it is a leap year?
+         * answer_1: An expression is defined using a ternary logical operator to check whether the year is a leap year.
+         * question_2: Does the expression evaluate to true for the year 2020?
          * answer_2: Yes, the expression evaluates to true.
-         * question_3: What value will be assigned to the variable leapYear?
-         * answer_3: The variable leapYear is assigned the string "Leap Year".
-         * question_4: What does the program print?
+         * question_3: What value will be assigned to the variable leapYear given that 2020 is a leap year?
+         * answer_3: The string value "Leap Year" is assigned to the string variable leapYear.
+         * question_4: What does the program print to the standard output device, i.e., the computer display or screen?
          * answer_4: The program prints "Leap Year".
          */
         /**
-         * stm_comment: The "OR" operator is true if any of the conditions satisfy whereas the "AND" operator is true if and only if both conditions satisfy.
-         * question_1: What is the difference between the "OR" and "AND" operators?
-         * answer_1: The "OR" operator is true if any of the conditions satisfy whereas the "AND" operator is true if and only if both conditions satisfy.
+         * stm_comment: A ternary logical operator is used to check whether the value of the variable year satisfies the definition of a leap year. The ternary logical operator has three elements: the condition, the result value if the condition is TRUE, and the result value if the condition is FALSE.
+         * question_1: How exactly is the specified year checked whether it is a leap year?
+         * answer_1: A ternary logical operator is used to check whether the value of the variable year satisfies the definition of a leap year.
+         * question_2: How many main elements do the ternary operator have and what are those elements?
+         * answer_2: The ternary logical operator has three elements: the condition, the result value if the condition is TRUE, and the result value if the condition is FALSE.
          */
         leapYear = (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)) ? "Leap Year" : "Not a Leap year";
         System.out.println(leapYear);
