24470a98e0e3ccc99a4835227d994b4b2416cffc
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-18 07:16:06 -0500

    Update SmallestDivisor.java
    Updated with Dr Rus second Feedback

--- miniDeepCodeSet/09.WhileLoops/SmallestDivisor.java
+++ miniDeepCodeSet/09.WhileLoops/SmallestDivisor.java
@@ -12,30 +12,32 @@ public class SmallestDivisor {
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_1: Define variables to represent a number and its divisor.
+         * logical_step_1: It defines the needed variables to represent a number and its divisor.
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: Variable num represents the positive number whose smallest divisor is to be found. The value of the variable num is initialized to 15. Variable divisor represents the smallest divisor of the given positive number.
+         * logical_step_details: The variable num represents the positive number whose smallest divisor is to be found. The variable num is initialized to 15. Variable divisor is initialized to 2, the smallest possible divisor (other than 1).
          * question_1: What does variable num represent?
          * answer_1:  Variable num represents the positive number whose smallest divisor is to be found.
          * question_2: What value is variable num initialized?
-         * answer_2: The value of variable num is initialized to 15.
+         * answer_2: The variable num is initialized to 15.
          * question_3: What does variable divisor represent?
-         * answer_3: Variable divisor represents the smallest divisor of the given positive number.
+         * answer_3: Variable divisor is initialized to 2, the smallest possible divisor (other than 1).
          */
         int num = 15;
         int divisor = 2;
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_2: Find the smallest divisor of the number num and print it.
+         * logical_step_2: It finds the smallest divisor of the number num and prints it.
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: A while loop generates a potential divisor candidate from 2 to the number itself. The while loop ends when the remainder of the division between num and divisor is zero, which means the value of divisor is the smallest divisor of num. At each iteration, the divisor's value is incremented by 1.
-         * question_1: What does the while loop do?
-         * answer_2: A while loop generates a potential divisor candidate from 2 to the number itself.
-         * question_2: When does the while loop end?
-         * answer_2: The while loop ends when the remainder of the division between num and divisor is zero, which means the value of divisor is the smallest divisor of num. 
-         * question_3: What happens at each iteration??
-         * answer_3: At each iteration, the divisor's value is incremented by 1.
+         * logical_step_details: A while loop is used to generate potential divisor candidates from 2 to the number itself. At each iteration, the divisor's value is incremented by 1 so that the next value is tested. The while loop ends when the remainder of the division between num and divisor is zero, which means the value of divisor is the smallest divisor of num. The "Smallest divisor of 15 is 3" is printed.
+         * question_1: How are potential divisor values generated and what is the range of candidate values?
+         * answer_1: A while loop is used to generate potential divisor candidates from 2 to the number itself.
+         * question_2: What happens to variable divisor at each iteration?
+         * answer_2: At each iteration, the divisor's value is incremented by 1 so that the next value is tested.
+         * question_3: When does the while loop end?
+         * answer_3: The while loop ends when the remainder of the division between num and divisor is zero, which means the value of divisor is the smallest divisor of num. 
+         * question_4: What value is printed to the standard output device, i.e., the screen?
+         * answer_4: The "Smallest divisor of 15 is 3" is printed.
          */
         /**
          * stm_comment: The While loop runs as long as the rest of the dividing num by the divisor is different from zero. 
