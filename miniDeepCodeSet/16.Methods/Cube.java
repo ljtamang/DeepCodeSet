df42cf9f7d4c0fa9feb18c6236aeed29a1678aae
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-18 13:11:54 -0500

    Update Cube.java
    Merged Dr Rus first feedback

--- miniDeepCodeSet/16.Methods/Cube.java
+++ miniDeepCodeSet/16.Methods/Cube.java
@@ -12,11 +12,11 @@ public class Cube {
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_1: Declare variables to store the length and volume of the cube. 
+         * logical_step_1: It declares variables to store the length and volume of the cube. 
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: A variable sideLength of integer type is declared to represent the length of a side of the cube. The value of the variable sideLength is initialized to 3. The variable vol is declared to store the volume of the cube.
-         * question_1: What is the type of variable sideLength?
-         * answer_1: The variable sideLength is of integer type. 
+         * logical_step_details: A variable sideLength is declared to represent the length of a side of the cube. The value of the variable sideLength is initialized to 3. The variable vol is declared to store the volume of the cube.
+         * question_1: Why is variable sideLength declared?
+         * answer_1: A variable sideLength is declared to represent the length of a side of the cube. 
          * question_2: What value is the variable sideLength initialized to?
          * answer_2: The value of the variable sideLength is initialized to 3.
          * question_3: Why is variable vol declared?
@@ -27,20 +27,15 @@ public class Cube {
 
         /**
          * domain_model_question: What is the goal of the following block of code?
-         * logical_step_1: Computes the volume of cube and prints the value.
+         * logical_step_2: It calls a method vol and prints the returned value. 
          * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-         * logical_step_details: To calculate the volume of the cube, the program calls a method vol. The method vol returns the value 27. Therefore, the program prints 27.
+         * logical_step_details: To calculate the volume of the cube, the program calls a method vol with parameter sideLength which is 3.  The method vol returns the value of 27 which is assigned to the variable vol. Therefore, the program prints 27.
          * question_1: What does the program do to calculate the volume of the cube?
-         * answer_1: To calculate the volume of the cube, the program calls a method vol.
+         * answer_1: To calculate the volume of the cube, the program calls a method vol with parameter sideLength which is 3.
          * question_2: What value does method vol return?
          * answer_2: The method vol returns the value 27.
          * question_3: What does the program print?
          * answer_3: The program prints 27.
-         */
-        /**
-         * stm_comment: The value 3 is passed as a parameter value to the method cube. 
-         * question_1: What value is passed as a parameter value to method cube?
-         * answer_1: The value 3 is passed as a parameter value to the method cube. 
          */
         vol = Cube.vol(sideLength);
         System.out.println(vol);
@@ -49,13 +44,15 @@ public class Cube {
 
     /**
      * domain_model_question: What is the goal of the following block of code?
-     * logical_step_3: Defines a method vol to calculate the volume of a cube.
+     * logical_step_3: It defines a method vol to calculate the volume of a cube.
      * program_model_question: Can you explain in detail how the goal of the current block of code is actually implemented?
-     * logical_step_details: A static method vol with int x as parameter is defined. The method vol computes and returns the volume of a cube.
-     * question_1: What is defined?
-     * answer_1: A static method vol with int x as parameter is defined.
-     * question_2: What does the method vol do?
-     * answer_2: The method vol computes and returns the volume of a cube.
+     * logical_step_details: A method vol is defined to compute and return the volume of a cube. The parameter x of method vol represents the side of the cube. The volume is computed by multiplying the x, side of a cube, itself for 3 times.
+     * question_1: Why is method vol defined?
+     * answer_1: A method vol is defined to compute and return the volume of a cube.
+     * question_2: What does the parameter x of method vol represent?
+     * answer_2: The parameter x of method vol represents the side of the cube.
+     * question_3: How is a volume of cube computed?
+     * answer_3: The volume is computed multiplying the x, side of a cube, itself for 3 times.
      */
     /**
      * stm_comment: The keyword static means it doesn't require a class object to call the method.	
