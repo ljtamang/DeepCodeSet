d5e203f37962ecee0f597f9d9b297fd3dd613e7f
Lasang Jimba Tamang <ljtamang@memphis.edu>
2021-10-28 10:52:14 -0500

    Create AreaOfCircle.java

---
+++ code/AreaOfCircle.java
@@ -0,0 +1,12 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+
+        final double PI = 3.14159;
+        double radius = 5.8;
+        double area;
+
+        area = radius * radius * PI;
+        System.out.printf("The area for the circle of radius %.2f is %.2f ", radius, area);
+
+    }
+}
